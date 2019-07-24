9f197ae8ea71580a1a91cf767a1da945154ec967
chaoyongmingyue <50563508+chaoyongmingyue@users.noreply.github.com>
2019-07-24 14:30:13 +0800

    Update welcome.go

--- welcome.go
+++ welcome.go
@@ -1,3 +1,19 @@
+import (
+	"testing"
+	"web3.go/web3"
+	"web3.go/web3/providers"
+)
+
 func TestThkCashCheck(t *testing.T) {
-	t.Log("welcome thinkey-dev/web3.go"
+	var err error
+	var connection = web3.NewWeb3(providers.NewHTTPProvider("192.168.1.13:8089", 10, false))
+	from := "0x2c7536e3605d9c16a7a3d7b1898e529396a65c23"
+
+	nonce, err := connection.Thk.GetNonce(from, "2")
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	t.Log("nonce:", nonce)
+
 }
